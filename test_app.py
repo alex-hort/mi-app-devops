42e5634f00c58fd7c3fd4c82f43b28d2eeeffd89
Alexis Horteales <ahortealese001@alumno.uaemex.mx>
2026-05-31 11:00:37 -0600

    fix: sys.path para pytest en Cloud Build

--- test_app.py
+++ test_app.py
@@ -1,15 +1,20 @@
 import pytest
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(__file__))
+
 from app import app
 
 @pytest.fixture
 def client():
-  app.config["TESTING"] = True
-  return app.test_client()
+    app.config["TESTING"] = True
+    return app.test_client()
 
 def test_home(client):
-  res = client.get("/")
-  assert res.status_code == 200
+    res = client.get("/")
+    assert res.status_code == 200
 
 def test_health(client):
-  res = client.get("/health")
-  assert res.json["healthy"] == True
+    res = client.get("/health")
+    assert res.json["healthy"] == True
